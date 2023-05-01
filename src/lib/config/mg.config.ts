cc01647056cc6987075a965e04e86a55c1a042e2
putmana <adam@ptmn.io>
2023-05-01 11:39:06 -0600

    Tweaked configuration for staging

--- src/lib/config/mg.config.ts
+++ src/lib/config/mg.config.ts
@@ -1,6 +1,6 @@
 // NO TRAILING SLASH
 // export const BASE_API_URL = "https://marigoldapi_s.ptmn.io/api/";
-export const BASE_API_URL = "https://marigoldapi.ptmn.io/api/"
+export const BASE_API_URL = "https://marigoldapi_s.ptmn.io/api/"
 
 export const API_VERSION = "v1";
 
