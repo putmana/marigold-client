9995d52805400af54b2ea1807b0ce3a365db0254
putmana <adam@ptmn.io>
2023-05-01 11:12:45 -0600

    Enabled static adapter

--- src/lib/config/mg.config.ts
+++ src/lib/config/mg.config.ts
@@ -1,5 +1,6 @@
 // NO TRAILING SLASH
-export const BASE_API_URL = "http://localhost/api/";
+// export const BASE_API_URL = "https://marigoldapi_s.ptmn.io/api/";
+export const BASE_API_URL = "http://localhost/api/"
 
 export const API_VERSION = "v1";
 
