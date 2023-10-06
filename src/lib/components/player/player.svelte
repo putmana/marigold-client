a2d4b405ea772503116bb3610e0ba731026f61aa
putmana <adam@ptmn.io>
2023-10-06 00:14:30 -0600

    Improved animations for interactive objects

--- src/lib/components/player/player.svelte
+++ src/lib/components/player/player.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import { defaultColor } from "$lib/scripts/color-engine/color-engine"
-	import { tracks, selectedTrackID } from "$lib/scripts/stores/LibraryStore"
+	import { tracks, selectedTrackID, albums, covers } from "$lib/scripts/stores/LibraryStore"
 
-	$: audio = $tracks.get($selectedTrackID)?.file
-
-	$: color = defaultColor
+	$: track = $tracks.get($selectedTrackID)
+	$: album = $albums.get(track?.albumID)
+	$: cover = $covers.get(album?.coverID)
+	$: color = cover?.palette ?? defaultColor
 </script>
 
 <section
 	class="wrapper"
-	style={`--main-light: ${color.main.light}; --main-dark: ${color.main.dark};`}
+	style={`--main-light: ${color.main.light}; --main-dark: ${color.main.dark}; --border-dark: ${color.border.dark};`}
 >
-	<audio src={audio} controls />
-	<div class="inner-wrapper" />
+	<div class="inner-wrapper">
+		<audio src={track?.file} />
+	</div>
 </section>
 
 <style lang="scss">
@@ -22,34 +24,17 @@
 
 	.wrapper {
 		position: fixed;
-		bottom: 10px;
-		left: 10px;
-		right: 10px;
+		inset: auto 0px 0px 0px;
 		height: 60px;
-		border-radius: 5px;
-		background-image: linear-gradient(to right, var(--main-light), var(--main-dark));
-		box-shadow: 0px 0px 15px colors.$shadow;
+		background-image: linear-gradient(to left, var(--main-light), var(--main-dark));
+		box-shadow: 0px 0px 20px colors.$shadow;
 
 		// Hack to render border color properly
 		.inner-wrapper {
 			height: inherit;
 			width: inherit;
-			border: 1px solid colors.$border-hover;
-			border-radius: 5px;
+			border-top: 1px solid var(--border-dark);
 			box-sizing: border-box;
-		}
-
-		@media (min-width: sizes.$screen-lg) {
-			bottom: 0px;
-			left: 0px;
-			right: 0px;
-			border-radius: 0px;
-
-			.inner-wrapper {
-				border: unset;
-				border-radius: 0px;
-				border-top: 1px solid colors.$border-hover;
-			}
 		}
 	}
 </style>
