b840ada1ad96935808d065898ea595a043b95bde
putmana <adam@ptmn.io>
2023-11-08 04:31:54 -0700

    Simplified logout process

--- src/lib/modes/auth/auth-logout.svelte
+++ src/lib/modes/auth/auth-logout.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
 	import { onMount } from "svelte"
-	import { initialized, playerController } from "$lib/scripts/stores/PlayerStore"
-	import {
-		library,
-		selectedAlbumID,
-		selectedPlaylistID,
-		selectedTrackID
-	} from "$lib/scripts/stores/LibraryStore"
-	import { mode } from "$lib/scripts/stores/ModeStore"
 	import { pb } from "$lib/scripts/database/pocketbase"
 
-	$: if (!$initialized) {
-		library.clear()
-
-		$selectedAlbumID = ""
-		$selectedTrackID = ""
-		$selectedPlaylistID = ""
-
-		$mode = "PLAYLISTS"
-		pb.authStore.clear()
-	}
-
 	onMount(() => {
-		playerController.resetQueue()
+		pb.authStore.clear()
+		location.reload()
 	})
 </script>
